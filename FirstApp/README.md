5134dfbaa76f16bdc9e7431dbecc14f0c2bc9ce2
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-03-29 17:43:33 -0500

    Request query

--- FirstApp/README.md
+++ FirstApp/README.md
@@ -6,3 +6,6 @@
 -Express Routing Basics 
     Routing take request in a path that is requested and matching it to some code and some response
 -Path parameters
+-Request query
+    Express construct it for us 
+    It is an object that we can acces and use it
--- FirstApp/index.js
+++ FirstApp/index.js
@@ -16,13 +16,13 @@ app.get("/", (req, res) => {
 
 app.get("/r/:subreddit", (req, res) => {
     //console.log(req.params);
-    const {subreddit} = req.params;
+    const { subreddit } = req.params;
     res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
 });
 
 app.get("/r/:subreddit/:postId", (req, res) => {
     //console.log(req.params);
-    const {subreddit, postId} = req.params;
+    const { subreddit, postId } = req.params;
     res.send(`<h1>viewing post id ${postId} on subreddit ${subreddit}</h1>`)
 });
 
@@ -42,10 +42,24 @@ app.post("/cats", (req, res) => {
     res.send("Post request to /cats! \n This is not the same as a get request");
 });
 
+app.get("/search", (req, res) => {
+    //console.log(req.query)
+    const { q } = req.query;
+    if (!q) {
+        res.send("nothing found if nothing search")
+    }
+    else {
+        res.send(`<h1>Search result for ${q}</h1>`)
+    }
 
-app.get("*", (req, res) =>{
+});
+
+app.get("*", (req, res) => {
     res.send("this is a generic response for all gets requests\n do not know tha path")
 });
+
+
+
 
 app.listen(3000, () => {
     console.log("listening in port 3000...");
